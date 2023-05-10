dd2c11e973515b8a851724b99180cb1ee2ed89d7
Lucas Fenaux <lprfenau@growl.ripple-domain.cs.uwaterloo.ca>
2023-05-10 15:33:07 +0000

    fixing description

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 setup(
     name='torch-gaggle',
     packages=find_packages(include=['gaggle*']),
-    version='0.0.1',
-    description='Testing gaggle pip packaging',
+    version='0.0.2',
+    description='Gaggle: Genetic Algorithms on the GPU using PyTorch',
     author='Lucas Fenaux',
     license='MIT',
     install_requires=[
